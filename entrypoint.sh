4d85416b028621a3f102c88f072a9a1496f14232
Dylan Anthony <contact@dylananthony.com>
2021-10-08 21:24:26 -0600

    refactor: Put bash logic in bash script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,16 @@
-#!/bin/sh -l
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
 
-ls /github
-ls /github/workspace
+generator_name=$1
+config_file=$2
+openapi_file=$3
+openapi_url=$4
+
+document=$openapi_url && [[ $openapi_url = "UNSET" ]] && document="/github/workspace/${openapi_file}"
+docker run -u 1001 --rm --workdir /github/workspace -v "$GITHUB_WORKSPACE":"/github/workspace" \
+  openapitools/openapi-generator-cli generate \
+  -i "$document" \
+  -g "$generator_name" \
+  -c "/github/workspace/$config_file" \
+  -o "/github/workspace/${generator_name}-client"
