512e607c4ca7676cb1df6dc139d33d694d099dbf
Dylan Anthony <contact@dylananthony.com>
2021-10-08 21:42:50 -0600

    feat: Add ability to select a specific Docker tag of the generator.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,11 @@ generator_name=$1
 config_file=$2
 openapi_file=$3
 openapi_url=$4
+generator_tag=$5
 
 document=$openapi_url && [[ $openapi_url = "UNSET" ]] && document="/github/workspace/${openapi_file}"
 docker run -u 1001 --rm --workdir /github/workspace -v "$GITHUB_WORKSPACE":"/github/workspace" \
-  openapitools/openapi-generator-cli generate \
+  "openapitools/openapi-generator-cli@${generator_tag}" generate \
   -i "$document" \
   -g "$generator_name" \
   -c "/github/workspace/$config_file" \
