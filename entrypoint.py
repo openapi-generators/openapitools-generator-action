c237162cec2b9f5db69ca970855e99dacd0b46c3
Rodrigo Cebrian Gonzalez <rodrigocebriang@gmail.com>
2022-02-20 02:34:58 +0100

    template-dir as new action input

--- entrypoint.py
+++ entrypoint.py
@@ -2,7 +2,7 @@ from subprocess import call
 from sys import argv
 from os import getenv
 
-(_, generator, generator_tag, openapi_file, openapi_url, config_file, *args) = argv
+(_, generator, generator_tag, openapi_file, openapi_url, config_file, template_dir, *args) = argv
 
 cmd = f"docker run -u 1001 --rm --workdir /github/workspace -v {getenv('GITHUB_WORKSPACE')}:/github/workspace"
 cmd = f"{cmd} openapitools/openapi-generator-cli:{generator_tag} generate"
@@ -15,6 +15,9 @@ else:
 
 if config_file != "UNSET":
     cmd = f"{cmd} -c /github/workspace/{config_file}"
+
+if template_dir != "UNSET":
+    cmd = f"{cmd} -t /github/workspace/{template_dir}"
 
 if args:
     cmd = f"{cmd} {' '.join(args)}"
