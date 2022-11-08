d77f0d87ef403e7c067c1f657437bf389d8b068a
ddeath <dusan.plavak@gmail.com>
2022-11-08 22:16:55 +0800

    Add Python2 support

--- entrypoint.py
+++ entrypoint.py
@@ -1,26 +1,36 @@
 from subprocess import call
-from sys import argv
 from os import getenv
+import argparse
 
-(_, generator, generator_tag, openapi_file, openapi_url, config_file, template_dir, *args) = argv
+parser = argparse.ArgumentParser(description='Generate openapi SDKs.')
+parser.add_argument('generator', help='name of openapi supported generator')
+parser.add_argument('generator_tag')
+parser.add_argument('openapi_file')
+parser.add_argument('openapi_url')
+parser.add_argument('config_file')
+parser.add_argument('template_dir')
+parser.add_argument('command_args', nargs = argparse.REMAINDER)
 
-cmd = f"docker run -u 1001 --rm --workdir /github/workspace -v {getenv('GITHUB_WORKSPACE')}:/github/workspace"
-cmd = f"{cmd} openapitools/openapi-generator-cli:{generator_tag} generate"
-cmd = f"{cmd} -g {generator} -o /github/workspace/{generator}-client"
+args = parser.parse_args()
 
-if openapi_url == "UNSET":
-    cmd = f"{cmd} -i /github/workspace/{openapi_file}"
+cmd = "docker run -u 1001 --rm --workdir /github/workspace -v {workspace}".format(workspace=getenv('GITHUB_WORKSPACE', '/github/workspace'))
+
+cmd = "{cmd} openapitools/openapi-generator-cli:{generator_tag} generate".format(cmd=cmd, generator_tag=args.generator_tag)
+cmd = "{cmd} -g {generator} -o /github/workspace/{generator}-client".format(cmd=cmd, generator=args.generator)
+
+if args.openapi_url == "UNSET":
+    cmd = "{cmd} -i /github/workspace/{openapi_file}".format(cmd=cmd, openapi_file=args.openapi_file)
 else:
-    cmd = f"{cmd} -i {openapi_url}"
+    cmd = "{cmd} -i {openapi_url}".format(cmd=cmd, openapi_url=args.openapi_url)
 
-if config_file != "UNSET":
-    cmd = f"{cmd} -c /github/workspace/{config_file}"
+if args.config_file != "UNSET":
+    cmd = "{cmd} -c /github/workspace/{config_file}".format(cmd=cmd, config_file=args.config_file)
 
-if template_dir != "UNSET":
-    cmd = f"{cmd} -t /github/workspace/{template_dir}"
+if args.template_dir != "UNSET":
+    cmd = "{cmd} -t /github/workspace/{template_dir}".format(cmd=cmd, template_dir=args.template_dir)
 
-if args:
-    cmd = f"{cmd} {' '.join(args)}"
+if args.command_args:
+    cmd = "{cmd} {command_args}".format(cmd=cmd, command_args=' '.join(args.command_args))
 
 # Call the command and return the exit code
 exit(call(cmd, shell=True))
